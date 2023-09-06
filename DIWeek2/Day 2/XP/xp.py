6ef50e579bbb00ee6e5eb067032ed4f8c289c6ea
Aaron Lewis <alewy00@gmail.com>
2023-09-06 21:15:42 +0300

    day 4 finished

---
+++ DIWeek2/Day 2/XP/xp.py
@@ -0,0 +1,41 @@
+# my_fav_num = set({0})
+# my_fav_num.add(1)
+# my_fav_num.add(2)
+# friend_fav_num = set({7})
+# all = my_fav_num | friend_fav_num
+# print(all)
+
+#2
+
+#You can append another tuple onto it - thats the only way
+
+#3
+
+# basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+
+
+# basket.remove("Banana")
+# basket.remove("Blueberries")
+# basket.append("Kiwi")
+# basket.insert(0, "Apples")
+# basket.count("Apples")
+# basket.clear()
+# print(basket)
+
+#4
+#floats can have decimals, intergers are whole
+# i = float(1)
+# list =[]
+# while i < 5:
+#     i += .5
+#     list.append(i)
+# print(list)
+
+#5
+
+# for i in range(0,20):
+#     print(i + 1)
+
+for i in range(1,21):
+   if(i % 2 == 0):
+      print(i)
